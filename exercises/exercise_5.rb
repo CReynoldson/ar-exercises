167444efe588479dab5cf08617d1cc753249c5c2
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-08 21:42:14 +0000

    got rid of instructions at the top

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,3 @@
-# 1. Output the total revenue for the entire company (all stores), using
-# Active Record's `.sum` calculation method.
-# 2. On the next line, also output the average annual revenue for all stores.
-# 3. Output the number of stores that are generating $1M or more in annual sales.
-# **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
-
-
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
