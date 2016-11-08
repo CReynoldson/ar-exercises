1915a8683eaf067d97de0c8a16e5a446f70d57b7
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-08 21:22:48 +0000

    found and looped through men and women's apparel stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,3 +1,5 @@
+### Exercise 4: Loading a subset of stores
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -7,3 +9,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end
+puts ""
+
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.each do |store|
+  if store.annual_revenue < 1000000
+    puts "#{store.name} #{store.annual_revenue}"
+  else
+    next
+  end
+end
